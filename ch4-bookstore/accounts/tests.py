868690d872a5fde61f2549eac866199bb60174f8
Lionel <minato.nami.dev@gmail.com>
2025-10-07 10:17:40 +0800

    Create user registration/login model/view

--- ch4-bookstore/accounts/tests.py
+++ ch4-bookstore/accounts/tests.py
@@ -1,4 +1,9 @@
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .forms import CustomUserCreationForm
+
+from .views import SignUpPageView
+
 from django.test import TestCase
 
 
@@ -30,4 +35,22 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_superuser)
 
 
-# Create your tests here.
+class SignUpViewTests(TestCase):
+    def setUp(self):
+        url = reverse("signup")
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "registration/signup.html")
+        self.assertContains(self.response, "Sign up")
+        self.assertNotContains(self.response, "Hi there! I should not be on the page.")
+
+    def test_signup_form(self):
+        form = self.response.context.get("form")
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, "csrfmiddlewaretoken")
+
+    def test_signup_view(self):
+        view = self.response.context.get("view")
+        self.assertEqual(view.__class__, SignUpPageView)
